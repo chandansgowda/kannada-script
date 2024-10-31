654f6231efc3032bec6223bc9ead63c4a5c764b5
chandansgowda <chandansuresh007@gmail.com>
2024-10-31 23:45:50 +0530

    fix: unused import

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import Script from "next/script";
 
 import { pageView } from "../helpers";
 
